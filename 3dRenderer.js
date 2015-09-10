b4ffb66649ea4d48f50413592b86529de60f6354
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2015-09-10 08:19:01 +0100

    Update 3dRenderer.js

--- 3dRenderer.js
+++ 3dRenderer.js
@@ -1,9 +1,9 @@
 //Raycasting demystified by http://www.playfuljs.com/a-first-person-engine-in-265-lines/
 
-map = {
-    algae: [],
+(Map = {
+    algaeGrid: undefined,
     size: NaN
-};
+}).algaeGrid = new Uint8Array(Map.size * Map.size);
 onMessage = function(e) {
     
 };
