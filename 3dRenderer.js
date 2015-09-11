94f185a0078c27e5607466f59abd4c481993c0f7
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2015-09-11 14:17:31 +0100

    Update 3dRenderer.js

--- 3dRenderer.js
+++ 3dRenderer.js
@@ -1,4 +1,5 @@
 //Raycasting demystified by http://www.playfuljs.com/a-first-person-engine-in-265-lines/
+//Didn't read their source code though! This was all hand-coded in the browser.
 
 (Map = {
     algaeGrid: undefined,
@@ -9,8 +10,24 @@ Player = {
     y: NaN,
     direction: NaN
 }
+Frame = undefined;
 onMessage = function(e) {
     switch(e.data[0]) {
-        case "move":
+        case "stateChange":
+            render();
+            break;
+        case "getFrame":
+            postMessage(["draw", Frame]);
+            break;
     }
+};
+
+render = function() {
+    var rays = [];
+    for (var a = 0; a < Frame.width; a++) {
+        rays[a] = new Ray(90-(a*(180/Frame.width)))
+    }
+};
+Ray = function(direction) {
+    
 };
